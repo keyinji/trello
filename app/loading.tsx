d0feeb8e12be2f0f3b9549fd9e525cf77ded6edc
keyinji <92763672+keyinji@users.noreply.github.com>
2023-08-13 21:51:13 -0400

    ok

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,4 @@
-import React from 'react'
-
-function loading() {
-  return (
-    <div>loading...</div>
-  )
-}
-
-export default loading
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return <p className="font-bold text-2xl items-center justify-center flex">Loading...</p>
+  }
